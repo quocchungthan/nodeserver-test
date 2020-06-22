4637daeeb0bf5d10e01dc9222ce3f2a92909aec1
Chung Than Quoc <chung.thanquoc@axonactive.com>
2020-06-22 13:42:28 +0700

    fix service not registered

--- trello/services/index.ts
+++ trello/services/index.ts
@@ -1,3 +1,4 @@
 export { TaskHistoryMongoService } from "./mongo-layer/TaskHistoryMongoService";
 export { TaskHistoryService } from "./TaskHistoryService";
+export { TaskService } from "./TaskService";
 export { TrelloApiService } from "./TrelloApiService";
